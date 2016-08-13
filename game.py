4155ca38431c17a71878996cfa2734726a9c70ab
Matthew Miller <mnmiller1@me.com>
2016-08-13 14:04:29 +1000

    Start work on player spawns

--- game.py
+++ game.py
@@ -2,19 +2,36 @@ import generation
 import uuid
 
 
+class Map:
+    def __init__(self, generator):
+        self.grid = generator.export_array_grid()
+        self.generator = generator
+
+    def is_valid_player_spawn(self, x, y):
+        pass
+
+
 class Game:
-    def __init__(self, name, player):
-        self.name = name
+    def __init__(self, player):
         generate = generation.Generator(30, 20)
-        self.map = generate.export_array_grid()
+        self.map = Map(generate)
         self.players = [player]
         self.id = uuid.uuid1()
+        self.pending = True
 
     def add_player(self, player):
-        self.players.append(player)
+        if self.pending:
+            self.players.append(player)
+            return True
+        else:
+            return False
 
     def setup(self):
-        pass
+        # Find the first team.
+        for x in range(self.map.generator.width):
+            for y in range(self.map.generator.height):
+                if self.map.is_valid_player_spawn(x, y):
+                    pass
 
 
 class GamePlayer:
