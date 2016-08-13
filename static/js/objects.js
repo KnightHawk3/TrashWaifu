e223dc26034c91742045858d6dbcebcff589af39
Steven Payne <steven.g.payne@hotmail.com>
2016-08-13 12:01:47 +1000

    basic waifu code, mostly getters

--- static/js/objects.js
+++ static/js/objects.js
@@ -1,15 +1,23 @@
-var GameObject = function(xpos, ypos, mvd, stats){
+var Waifu = function(name, totalHealth, element, attack, defence, range, mvd, xpos, ypos){
   this.pos = { x: xpos, y: ypos };
   this.goalpos = { x: xpos, y: ypos };
   this.movedistance = mvd;
-  this.stats = stats;
+  //this.sprite = null;
+
+  this.name = name;
+  this.maxHealth = totalHealth;
+  this.currentHealth = totalHealth;
+  this.element = element;
+  this.attack = attack;
+  this.defence = defence;
+  this.range = range;
 }
 
-GameObject.prototype.getMoveDistance = function(){
+Waifu.prototype.getMoveDistance = function(){
   return this.movedistance;
 }
 
-GameObject.prototype.setMoveTo = function( xpos, ypos ){
+Waifu.prototype.setMoveTo = function( xpos, ypos ){
   var distanceToMove = Math.abs(this.pos.x - xpos) + Math.abs(this.pos.y - ypos);
   if( distanceToMove <= this.movedistance ){
     this.goalpos.x = xpos;
@@ -20,9 +28,9 @@ GameObject.prototype.setMoveTo = function( xpos, ypos ){
   }
 }
 
-GameObject.prototype.moveStep = function(){
-  var xdif = this.goalpos.x - this.pos.x
-  var ydif = this.goalpos.y - this.pos.y
+Waifu.prototype.moveStep = function(){
+  var xdif = this.goalpos.x - this.pos.x;
+  var ydif = this.goalpos.y - this.pos.y;
 
   if( xdif != 0 ){
     this.pos.x += Math.sign(xdif);
@@ -31,4 +39,49 @@ GameObject.prototype.moveStep = function(){
   }
 
   return this.pos;
+}
+
+Waifu.prototype.takeDamage = function(damage) {
+  this.currentHealth -= damage;
+  if(this.currentHealth <= 0) {
+    killWaifu();
+  }
+}
+
+Waifu.prototype.killWaifu = function() {
+  //destroy object, play animation?
+}
+
+
+// Waifu.prototype.setSprite = function(path){
+//   //load texture from file
+//
+// }
+//
+// Waifu.prototype.getSprite = function() {
+//   return this.sprite;
+// }
+
+Waifu.prototype.getName = function(){
+  return this.name;
+}
+
+Waifu.prototype.getElement = function() {
+  return this.element;
+}
+
+Waifu.prototype.getAttack = function(){
+  return this.attack;
+}
+
+Waifu.prototype.getDefence = function(){
+  return this.defence;
+}
+
+Waifu.prototype.getCurrentHealth = function(){
+  return this.getCurrentHealth;
+}
+
+Waifu.prototype.getMaxHealth = function(){
+  return this.maxHealth;
 }
