515925636c4f436fd7508c8402ce007898fef390
Blaid Lee <blaid.lee@gmail.com>
2016-08-13 11:38:08 +1000

    Attempting basic sockets

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ def default_error_handler(e):
     print(request.event["message"]) # "my error event"
     print(request.event["args"])    # (data,)
 
-@socketio.on('')
+@socketio.on('connect')
 def on_connect():
     if current_user.is_authenticated:
         emit('login', {'message': True})
     else:
+        print("testgoku");
         emit('login', {'message': False})
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
+var socket = io();
 var anim = new Animation();
 anim.addCharacter(200, 150, 'static/images/0.png');
 anim.addCharacter(300, 150, 'static/images/5.png');
 anim.startAnim();
+
+socket.on('login', function(data){
+  console.log(data);
+})
