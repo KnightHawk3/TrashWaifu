35dd167241ab1c968b4e5a5199963471884246af
Blaid Lee <blaid.lee@gmail.com>
2016-08-13 11:22:51 +1000

    Animation stuff

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,3 +1,42 @@
 var Animation = function(){
-  this.renderer = new PIXI.WebGLRenderer(screen.availWidth, screen.availHeight);
+  this.renderer = new PIXI.WebGLRenderer(1024, 768);
+  this.renderer.backgroundColor = 0x3498db;
+
+  document.body.appendChild(this.renderer.view);
+
+  this.stage = new PIXI.Container();
+
+  this.sprites = [];
+
+}
+
+Animation.prototype.startAnim = function(){
+  var self = this;
+
+  animate();
+
+  for(var i = 0, iLen = this.sprites.length; i < iLen; i++ ){
+    this.stage.addChild(this.sprites[i]);
+  }
+
+  function animate() {
+    requestAnimationFrame(animate);
+
+    self.sprites[0].rotation += 0.01;
+
+    self.renderer.render(self.stage);
+  }
+}
+
+Animation.prototype.addCharacter = function(x, y, textureURL){
+  var texture = PIXI.Texture.fromImage(textureURL);
+  var character = new PIXI.Sprite(texture);
+
+  character.anchor.x = 0.5;
+  character.anchor.y = 0.5;
+
+  character.position.x = x;
+  character.position.y = y;
+
+  this.sprites.push(character);
 }
---
+++ static/js/main.js
@@ -0,0 +1,4 @@
+var anim = new Animation();
+anim.addCharacter(200, 150, 'static/images/0.png');
+anim.addCharacter(300, 150, 'static/images/5.png');
+anim.startAnim();
